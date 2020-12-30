8cb2ad715fbd2edafc3962b92ab523183f211a0a
Shankari Thanikachalam <60396595+shankarithanikachalam@users.noreply.github.com>
2020-12-30 06:56:25 +0530

    Create Library.py

---
+++ Library.py
@@ -0,0 +1,81 @@
+import threading
+from threading import*
+import time
+d={}
+def create(key,value,timeout=0):
+    if key in d:
+        print("error : This key already exists in data store")
+    else:
+        if(key.isalpha()):
+            if((len(d)<1024*1024*1024)and(value<=16*1024*1024)):
+                if(timeout==0):
+                    l=[value,timeout]
+                else:
+                    l=[value,time.time()+timeout]
+                if(len(key)<=32):
+                    d[key]=l
+                print("This key is created")
+            else:
+                print("The size of datastore and value exceeds")
+        else:
+            print("error: invalid key name")
+
+def read(key):
+    if key not in d:
+        print("error: invalid key")
+    else:
+        b=d[key]
+        if (b[1]==0):
+            a=str(key)+":"+str(b[0])
+            return a
+        else:
+            if(time.time()<b[1]):
+                a=str(key)+":"+str(b[0])
+                return a
+            else:
+                print("error: Time to live of key has expired")
+
+def delete(key):
+    if key not in d:
+        print("error: Invalid key")
+    else:
+        b=d[key]
+        if(b[1]==0):
+            del d[key]
+            print("This key is deleted")
+        else:
+             if(time.time()<b[1]):
+                 del d[key]
+                 print("This key is deleted")
+             else:
+                print("error: Time to live of key has expired")
+
+def modify(key,value):
+    if key not in d:
+        print("error: Invalid key")
+    else:
+        b=d[key]
+        if(b[1]==0):
+            c=[]
+            c.append(value)
+            c.append(b[1])
+            d[key]=c
+            print("This key is modified")
+        else:
+            if(time.time()<b[1]):
+                c=[]
+                c.append(value)
+                c.append(b[1])
+                d[key]=c
+                print("This key is modified")
+            else:
+                print("error: Time to live of key has expired")
+
+            
+                 
+
+
+                
+
+                
+            
